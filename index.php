a66dcbaf75cc6b227406b753627e26d4ce6d47fd
maxxitope <maxxitope@outlook.com>
2014-09-17 20:38:04 -0500

    1
    primera prueba

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
+<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
+<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="">
+<!--<![endif]-->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Documento sin título</title>
+<link href="style/boilerplate.css" rel="stylesheet" type="text/css">
+<link href="/style/style.css" rel="stylesheet" type="text/css">
+<!-- 
+Para obtener más información sobre los comentarios condicionales situados alrededor de las etiquetas html en la parte superior del archivo:
+paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/
+  
+Haga lo siguiente si usa su compilación personalizada de modernizr (http://www.modernizr.com/):
+* inserte el vínculo del código js aquí
+* elimine el vínculo situado debajo para html5shiv
+* añada la clase "no-js" a las etiquetas html en la parte superior
+* también puede eliminar el vínculo con respond.min.js si ha incluido MQ Polyfill en su compilación de modernizr 
+-->
+<!--[if lt IE 9]>
+<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="style/respond.min.js"></script>
+</head>
+<body>
+<div class="gridContainer clearfix">
+  <div id="LayoutDiv1">Utilice el panel Insertar para obtener etiquetas div adicionales de diseño de cuadrícula fluida. Nota: Todas las etiquetas div de diseño deben insertarse directamente dentro de la etiqueta div 'gridContainer'. Actualmente no se admiten etiquetas div de diseño anidadas. </div>
+</div>
+<div>
+Utilice el panel Insertar para obtener etiquetas div adicionales de diseño de cuadrícula fluida. Nota: Todas las etiquetas div de diseño deben insertarse directamente dentro de la etiqueta div 'gridContainer'. Actjhjkjkjhkualmente no se admiten etiquetas div de diseño anidadas.
+</div>
+</body>
+</html>
